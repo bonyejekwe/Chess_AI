e035a43ceb2e6b90dff4e8d0ffc4218db9b71e69
maxwellarnold24 <70716256+maxwellarnold24@users.noreply.github.com>
2021-11-30 00:15:25 -0500

    Interacting with game done

--- Project/displayGame.py
+++ Project/displayGame.py
@@ -6,27 +6,9 @@
 
 from board import Board
 from AI import AI
+from pieces import *
 
 import pygame
-
-## Function used for performance testing
-def run_game(num=2, mode1="random", mode2="random"):
-    """Run game using num of AI's (1=player vs AI, 2=AI vs AI). Defaults to AI vs AI"""
-    game = Board()
-    if num == 1:  # player against ai
-        side = int(input("Select side you would like to play on (1 for white, -1 for black): "))
-        ai_team = -1 * side
-        ai = AI(ai_team, mode1)
-        ai2 = False
-    else:
-        ai = AI(1, mode1)
-        ai2 = AI(-1, mode2)
-        
-    game.start_game()
-    
-    game_winner = turn(game, ai, ai2)  # run the game
-    print("the winner is...", game_winner)
-    return game_winner
 
 
 # Class for playing the game
@@ -46,6 +28,18 @@ class playGame :
         self.width, self.height = 800, 800
         # Define the cell size
         self.cell_size = self.width / 8.0
+        
+        
+        # Define the possible pieces
+        self.pos_pieces = ['K', 'Q', 'B', 'R', 'N', 'P']
+
+        # Get the piece images
+        self.white_pieces = {self.pos_pieces[i] : pygame.transform.scale(pygame.image.load(r'Piece Images/{}/{}.png'.format(\
+                                        'white', self.pos_pieces[i])), (100, 100)) for i in range(len(self.pos_pieces))}
+
+        # Get the piece images
+        self.black_pieces = {self.pos_pieces[i] : pygame.transform.scale(pygame.image.load(r'Piece Images/{}/{}.png'.format(\
+                                        'black', self.pos_pieces[i])), (100, 100)) for i in range(len(self.pos_pieces))}
         
         if start_on_creation == True:
             self.start_game(AI_mode)
@@ -85,14 +79,13 @@ class playGame :
         self.game = Board()
     
         # Get the side from the player
-        #side = int(input("Select side you would like to play on (1 for white, -1 for black): "))
-        side = 1
+        self.side = int(input("Select side you would like to play on (1 for white, -1 for black): "))
+        #self.side = 1
+        
         # Set the ai's side
-        ai_team = -1 * side
+        ai_team = -1 * self.side
         # Define the ai
         ai = AI(ai_team, mode)
-        # Set ai2 variable to false
-        ai2 = False
 
         # Start the game
         self.game.start_game()
@@ -101,67 +94,177 @@ class playGame :
         self.initialize_board()
         
         # Start the first turn
-        game_winner = self.turn(self.game, ai, ai2)  # run the game
+        game_winner = self.master_function(ai)
         
         return game_winner
 
 
-
-    def turn(self, game, ai_game, ai_game2=False):  # effectively defaults to no AI for game
-        timed_out = -5
-        while game.is_game_over() is False and game.get_current_move_count() < 200:
-            move(game, ai_game, ai_game2)
-            game.switch_turn()
-            print(f"Turn #: {game.get_current_move_count()}...")
-            # print(f"captured: {[str(p) for p in game.get_captured()]}")
-            if game.is_game_over():
-                return game.winner()
-            else:
-                print('timed out')
-                return timed_out
-
-
-def move(game: Board, game_ai: AI, game_ai2=False):
-    if game.get_current_turn() == 1:
-        print("White's turn.")
-        mv_string = "Location of piece, ie A2: "
-        ds_string = "Desired position, ie A3: "
-    else:
-        print("Black's turn.")
-        mv_string = "Location of piece, ie A7: "
-        ds_string = "Desired position, ie A6: "
-
-    if game.is_in_check(game.get_current_turn()):
-        for _ in range(10):
-            print(f'{game.get_current_turn()} is in check!!!')
-
-    if game_ai.get_team() == game.get_current_turn():  # AI's turn
-        game_ai.all_legal_moves(game)
-        game_ai.make_move(game)
-    elif game_ai2:
-        if game_ai2.get_team() == game.get_current_turn():  # AI's turn
-            game_ai2.all_legal_moves(game)
-            game_ai2.make_move(game)
-    else:
-        # take positions from input
-        moving_piece = list(input(mv_string))
-        moving_piece[0] = moving_piece[0].upper()
-        moving_piece[1] = int(moving_piece[1])
-        moving_piece = tuple(moving_piece)
+    def master_function(self, game_ai):  # effectively defaults to no AI for game
+        ''' The graphics master function '''
+        # Define a dictionary of letters to map
+        letters_map = {0 : 'A', 1 : 'B', 2 : 'C', 3 : 'D', 4 : 'E', 5 : 'F', 6 : 'G', 7 : 'H'}
+                    
+        # Definte the game exit variable
+        game_exit = False
+            
+        # set display
+        gameDisplay = pygame.display.set_mode((self.width, self.height))
         
+        # Set the variable for if a new move has been chosen
+        piece_to_move_chosen = False
+
+        # Set the variable for the location of the chosen piece
+        chosen_piece = []
         
-        desired_position = list(input(ds_string))
-        desired_position[0] = desired_position[0].upper()
-        desired_position[1] = int(desired_position[1])
-        desired_position = tuple(desired_position)
-        # need error check tuples, a-h, 1-8, len = 2, custom exceptions
-        try:
-            game.move_piece(moving_piece, desired_position)
-            print("move successful")
-        except:  # This is far too general, but it is also not nearly finished
-            print(game.__repr__())
-            print('Redo move.')
-            move(game, game_ai, game_ai2)
+        # Check if the player's side is black, and if it is then have the AI make the first move
+        if self.side == -1:
+            pass
+        
+        # While the exit button has not been pressed
+        while not game_exit:
+
+            # Check the events
+            for event in pygame.event.get():
+                # If the event is quitting the applicaiton, then do so
+                if event.type == pygame.QUIT:
+                    game_exit = True
+                    
+                # If the event is clicking the mouse, then get the position
+                if event.type == pygame.MOUSEBUTTONUP and self.game.get_current_turn() == self.side:
+                    # Find the mouse position
+                    pos = pygame.mouse.get_pos()
+                    
+                    # Find the chess coordinates of the chosen position
+                    x, y = int(pos[0] / self.cell_size), int(pos[1] / self.cell_size)
+                    
+                    #print(x, y)
+                    
+                    # If this is the first piece we have chosen and it is a piece of our color
+                    if piece_to_move_chosen == False and isinstance(self.game.get_board()[y][x], Piece) == True and self.game.get_board()[y][x].get_color() == self.side:
+                                                
+                        # Change the background color of the cell
+                        pygame.draw.rect(self.board, self.chosen_color, (x*self.cell_size, y*self.cell_size, self.cell_size, self.cell_size))
+            
+                        # Change the variable to true
+                        piece_to_move_chosen = True
+            
+                        # Save the location of the chosen piece
+                        chosen_piece = [x, y]
+            
+                    # Check if a piece has been chosen and the selected tile does not match the chosen piece tile
+                    elif piece_to_move_chosen == True and (x != chosen_piece[0] or y != chosen_piece[1]):
+                        
+                        ## Check that the move was valid using game code ##
+                        try:
+                            self.game.move_piece(tuple([letters_map[chosen_piece[0]], 1+chosen_piece[1]]), 
+                                                 tuple([letters_map[x], 1+y]))
+                            #print("move successful")
+                        except:  # This is far too general, but it is also not nearly finished
+                            #print('Redo move.')
+                            # Exit out of current loop itteration
+                            continue
+                        
+                        # Find out which color should be used
+                        if (chosen_piece[0] + chosen_piece[1]) % 2 == 1:
+                            overwrite_color = self.black
+                        else:
+                            overwrite_color = self.white
+                            
+                        # Change the background color of the last location
+                        pygame.draw.rect(self.board, overwrite_color, (chosen_piece[0]*self.cell_size, chosen_piece[1]*self.cell_size, self.cell_size, self.cell_size))
+                        
+                        # set the piece_to_move_chosen to false
+                        piece_to_move_chosen = False
+                        
+                        
+                        # Change the current turn
+                        self.game.switch_turn()
+                            
+                        # Check if the game is over
+                        if self.game.is_game_over():
+                            
+                            return self.game.winner()  
+                        
+                    elif piece_to_move_chosen == True and (x == chosen_piece[0] and y == chosen_piece[1]):
+                        
+                        # Find out which color should be used
+                        if (chosen_piece[0] + chosen_piece[1]) % 2 == 1:
+                            overwrite_color = self.black
+                        else:
+                            overwrite_color = self.white
+                            
+                        # Change the background color of the last location
+                        pygame.draw.rect(self.board, overwrite_color, (chosen_piece[0]*self.cell_size, chosen_piece[1]*self.cell_size, self.cell_size, self.cell_size))
+                        
+                        # Reset the chosen piece variable
+                        piece_to_move_chosen = False
+                    
+            # Fill the display with the board
+            gameDisplay.blit(self.board, self.board.get_rect())
+            
+            # Draw the pieces on the board
+            self.draw_pieces(gameDisplay)
+            
+            # Update the display
+            pygame.display.update()
+            
+            # If its the computer's turn
+            if self.game.get_current_turn() == -1*self.side:
+                # Check if the game is over
+                if self.game.is_game_over():
+                    return self.game.winner()
+            
+                # Check if a player is in check
+                if self.game.is_in_check(self.game.get_current_turn()):
+                    for _ in range(10):
+                        print(f'{self.game.get_current_turn()} is in check!!!')
+                        
+                # Make the AI move
+                game_ai.all_legal_moves(self.game)
+                game_ai.make_move(self.game)
+                        
+                # Check if teh game is over
+                if self.game.is_game_over():
+                    return self.game.winner()
+                        
+                # Check if a player is in check
+                if self.game.is_in_check(self.game.get_current_turn()):
+                    for _ in range(10):
+                        print(f'{self.game.get_current_turn()} is in check!!!')
+                    
+                # Switch the turn
+                self.game.switch_turn()
+        
+        # Quit pygame
+        pygame.quit()
+        
+    def draw_pieces(self, gameDisplay):
+        ''' Blitzes the pieces onto the board'''
+        # Get the board
+        brd = self.game.get_board()
+                
+        # Iterating over rows
+        for y in range(len(brd)):
+            
+            # Iterating over the columns
+            for x in range(len(brd[y])):
+                
+                # Check that the element is a piece
+                if isinstance(brd[x][y], Piece) == True:
+                    
+                    # Find the piece type
+                    piece_type = str(brd[x][y])[0]
+                    
+                    # 1 is white and -1 is black
+                    piece_color = brd[x][y].get_color()
+                    
+                    # Draw the respective piece
+                    if piece_color == 1:
+                        gameDisplay.blit(self.white_pieces[piece_type], (self.cell_size*y, self.cell_size*x)) 
+                        
+                    elif piece_color == -1:
+                        gameDisplay.blit(self.black_pieces[piece_type], (self.cell_size*y, self.cell_size*x)) 
+
 
 
 def main():
